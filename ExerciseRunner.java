2447666fd2bba7672756775fbf1988c2ee12b951
Alexy845 <alexy.daubresse@ynov.com>
2023-02-06 09:59:56 +0100

    HelloWorld

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,5 +1,3 @@
-import exo.HelloWorld;
-
 public class ExerciseRunner {
     public static void main(String[] args) {
         System.out.println(HelloWorld.helloWorld());
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-package exo;
+
 public class HelloWorld {
     public static String helloWorld() {
         // your code here
