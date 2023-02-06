59a55f5bd846b04590be5768fd68a2f325e53c57
Alexy845 <alexy.daubresse@ynov.com>
2023-02-06 11:10:42 +0100

    StringContain

---
+++ StringContain.java
@@ -0,0 +1,13 @@
+public class StringContain {
+    public static boolean isStringContainedIn(String subString, String s) {
+        if (subString.length() > s.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length() - subString.length() + 1; i++) {
+            if (s.substring(i, i + subString.length()).equals(subString)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
