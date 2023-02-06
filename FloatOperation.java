e0d7dce27ba566664a91b26dd94a67dfa43afba8
Alexy845 <alexy.daubresse@ynov.com>
2023-02-06 10:11:28 +0100

    FloatOperation

---
+++ FloatOperation.java
@@ -0,0 +1,10 @@
+public class FloatOperation {
+    public static float addTwoFloats(float a, float b) {
+        float sum = a + b;
+        return sum;
+    }
+    public static float divideTwoFloats(float a, float b) {
+        float quotient = a / b;
+        return quotient;
+    }
+}
