439c240abfa973ac55bc086c5d73a5055b18e744
Alexy845 <alexy.daubresse@ynov.com>
2023-02-08 23:07:04 +0100

    FormatDate

--- FormatDate.java
+++ FormatDate.java
@@ -2,22 +2,35 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yyyy HH:mm:ss");
-        return dateTime.format(formatter);
+        if (dateTime == null) {
+            return null;
+        }
+        String dT = dateTime.format(DateTimeFormatter.ofPattern("'Le' dd MMM 'de l'\'an' yyyy 'à' HH'h'mm'm' 'et' ss's'", Locale.forLanguageTag("fr")));
+        return dT;
     }
-    
+
     public static String formatSimple(LocalDate date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return date.format(formatter);
+        if (date == null) {
+            return null;
+        }
+        String d = date.format(DateTimeFormatter.ofPattern("MMMM dd yy", Locale.forLanguageTag("it")));
+        return d;
     }
-
     public static String formatIso(LocalTime time) {
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_TIME;
-        return time.format(formatter);
-
+        if (time == null) {
+            return null;
+        }
+        if (time.getNano() == 0) {
+            String t = time.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            return t;
+        }
+        String t = time.format(DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS"));
+        return t;
     }
 
 }
