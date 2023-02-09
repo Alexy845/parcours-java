ed2054fc9f52d622b252c69b1cc0eee34d1b7363
Alexy845 <alexy.daubresse@ynov.com>
2023-02-09 15:59:09 +0100

    CelestialObject

--- CelestialObject.java
+++ CelestialObject.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 public class CelestialObject {
     public double x;
     public double y;
@@ -13,6 +15,33 @@ public class CelestialObject {
     public CelestialObject() {
         this("Soleil", 0, 0, 0);
     }
+
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+    public void setZ(double z) {
+        this.z = z;
+    }
     
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
