45ff17553baa186474e3c52c2fc7a7e620904e31
Alexy845 <alexy.daubresse@ynov.com>
2023-02-09 15:49:57 +0100

    CelestialObject

--- CelestialObject.java
+++ CelestialObject.java
@@ -4,4 +4,15 @@ public class CelestialObject {
     public double z;
     public String name;
     
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+    public CelestialObject() {
+        this("Unknown", 0, 0, 0);
+    }
+    
 }
+
