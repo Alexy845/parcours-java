cb0ea6072dc251ff7bb2643b426d18bf61d1a9cb
Alexy845 <alexy.daubresse@ynov.com>
2023-02-09 10:46:42 +0100

    DifferenceBetweenDate

---
+++ Sort.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Sort {
+
+    public static List<Integer> sort(List<Integer> list) {
+        if (list == null) {
+            return null;
+        } 
+        List<Integer> result = new ArrayList<>(list);
+        Collections.sort(result);
+        return result;
+    }
+
+    public static List<Integer> sortReverse(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
+        List<Integer> result = new ArrayList<>(list);
+        Collections.sort(result, Collections.reverseOrder());
+        return result;
+    }
+}
