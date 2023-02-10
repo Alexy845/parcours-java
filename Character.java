fbee11a6b5b3d559467359a5710bef57ab92f63e
Alexy845 <alexy.daubresse@ynov.com>
2023-02-10 17:17:55 +0100

    Adventure

--- Character.java
+++ Character.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
 
     private final int maxHealth;
     private int currentHealth;
     private final String name;
-
+    private static List <Character> allCharacters = new ArrayList<>();
+    
     public int getMaxHealth() {
         return maxHealth;
     }
@@ -18,6 +22,7 @@ public class Character {
         this.name = name;
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
+        allCharacters.add(this);
     }
 
     public void takeDamage(int damage) {
@@ -30,7 +35,39 @@ public class Character {
     public void attack (Character Uruk) {
         Uruk.takeDamage(9);
     }
+    public void allCharacters() {
+        allCharacters.add(this);
 
+    }
+    public static String printStatus() {
+        String miseEnPage = "------------------------------------------";
+        String res = miseEnPage+"\n"+"Characters currently fighting :";
+        if (allCharacters.isEmpty()){
+            return miseEnPage+"\n"+"Nobody's fighting right now !"+"\n"+miseEnPage ;
+        }
+        for (Character character : allCharacters) {
+            res = res +"\n - "+character ;
+        }
+        res = res +"\n"+miseEnPage;
+        return res;
+    }
+
+    public static Character fight(Character character1, Character character2) {
+        while (character1.getCurrentHealth() > 0 && character2.getCurrentHealth() > 0) {
+            character1.attack(character2);
+            
+        
+        if (character1.getCurrentHealth() > 0) {
+            return character1;
+        }
+        character2.attack(character1); 
+        if (character2.getCurrentHealth() > 0) {
+            return character2;
+            }
+        }
+        return null;
+    
+    }
 
     @Override
     public String toString() {
