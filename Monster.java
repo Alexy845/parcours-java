c16d99b65322b0264277a2c25b66309456c421f0
Alexy845 <alexy.daubresse@ynov.com>
2023-02-15 15:57:32 +0100

    abstrack monster fait

--- Monster.java
+++ Monster.java
@@ -5,18 +5,18 @@ public class Monster extends Character {
     }
     @Override
     public void takeDamage(int damage) {
-        if (damage > currentHealth){
+        if (damage > currentHealth) {
             currentHealth = 0;
-        }else{
-            int healthAfterDamage = getCurrentHealth() - (int) (0.8 * damage);
-            setCurrentHealth(healthAfterDamage);
-            currentHealth -= damage;
+        } else {
+            int damageAfterReduction = (int) Math.floor(damage * 0.8);
+            currentHealth -= damageAfterReduction;
         }
     }
+    
     @Override
-    public void attack (Character Uruk) {
-        Uruk.takeDamage(7);
-    }
+    public void attack(Character character) {
+        character.takeDamage(7);
+    } 
     @Override
     public String toString() {
         if (getCurrentHealth() > 0) {
