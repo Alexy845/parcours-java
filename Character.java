d7f8818260b0f9fe1e6ef624325729f413a26543
Alexy845 <alexy.daubresse@ynov.com>
2023-02-15 13:24:34 +0100

    Interface

--- Character.java
+++ Character.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Character {
 
     private final int maxHealth;
-    private int currentHealth;
+    protected int currentHealth;
     private final String name;
     private static List <Character> allCharacters = new ArrayList<>();
     
